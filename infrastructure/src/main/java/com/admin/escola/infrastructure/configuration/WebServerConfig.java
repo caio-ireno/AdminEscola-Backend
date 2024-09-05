29e10d72db597829b025c73acbb6332a90b56d7a
caio-ireno <caio_ireno@hotmail.com>
2024-09-05 17:23:35 -0300

    Feat: start with springframework and configurations

---
+++ infrastructure/src/main/java/com/admin/escola/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.admin.escola.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.admin.escola")
+public class WebServerConfig {
+}
